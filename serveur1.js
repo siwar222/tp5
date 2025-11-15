6a1ab09e41c4d16d3d5ca99a994cb128a3d39873
Siwar Souli <ghrety1234@gmail.com>
2025-11-15 19:42:05 +0100

    exercice 8

--- serveur1.js
+++ serveur1.js
@@ -1,13 +1,26 @@
 // serveur1.js (version minimale)
+//const http = require('http');
+
+//const PORT = 8080;
+
+///const server = http.createServer((req, res) => {
+  //res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  //res.end('Bonjour');
+//});
+
+//server.listen(PORT, () => {
+  //console.log(`Serveur démarré sur http://localhost:${PORT}`);
+//});
 const http = require('http');
 
-const PORT = 8080;
+// Récupérer le port passé en argument
+const port = process.argv[2] || 3000;  // 3000 si aucun port n'est donné
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  res.end('Bonjour');
+const serveur = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Serveur Node.js en marche !");
 });
 
-server.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+serveur.listen(port, () => {
+  console.log("Serveur démarré sur le port :", port);
 });
